e1be72617af77a007a16804cd036217fda3cd5c1
Jennifer Liu <jenniferliu1272@gmail.com>
2023-09-18 14:03:14 -0700

    Use DataGrid instead

--- src/app/components/MedicineTable.tsx
+++ src/app/components/MedicineTable.tsx
@@ -1,11 +1,5 @@
 import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 
 type MedicineTableProps = {
   query: string;
@@ -40,46 +34,55 @@ const rows = [
     count: 2,
     date: new Date(),
   }),
+  createData({
+    ID: 1220,
+    name: "Strattera",
+    count: 2,
+    date: new Date(),
+  }),
+  createData({
+    ID: 1221,
+    name: "Adderall",
+    count: 4,
+    date: new Date(),
+  }),
+  createData({
+    ID: 1222,
+    name: "dextroamphetamine",
+    count: 4,
+    date: new Date(),
+  }),
+];
+
+const columns: GridColDef[] = [
+  { field: "ID", headerName: "ID", width: 100 },
+  { field: "name", headerName: "Name", width: 100 },
+  { field: "count", headerName: "Count", width: 100 },
+  { field: "date", headerName: "Date", minWidth: 300 },
 ];
 
 function queryMatches({ query, ID, name }: MedicineType & MedicineTableProps) {
   return name.includes(query) || String(ID).includes(query) || query === "";
 }
 
+function filterRows(query: string) {
+  return rows.filter((row) => queryMatches({ query, ...row }));
+}
+
 export default function MedicineTable({ query }: MedicineTableProps) {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell align="right">Chinese Name</TableCell>
-            <TableCell align="right">Count</TableCell>
-            <TableCell align="right">Date</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => {
-            console.log("Query: " + query);
-            if (queryMatches({ ...row, query })) {
-              return (
-                <TableRow
-                  key={row.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.ID}
-                  </TableCell>
-                  <TableCell align="right">{row.name}</TableCell>
-                  <TableCell align="right">{row.count}</TableCell>
-                  <TableCell align="right">{row.date.toString()}</TableCell>
-                </TableRow>
-              );
-            }
-            return null;
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={filterRows(query)}
+        getRowId={(row) => row.ID}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+      />
+    </div>
   );
 }
