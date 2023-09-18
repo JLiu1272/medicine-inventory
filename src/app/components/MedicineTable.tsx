b3cf4492c65d87e6d4146bd2a474680bf4fd3654
Jennifer Liu <jenniferliu1272@gmail.com>
2023-09-18 15:45:17 -0700

    Add filtering feature

--- src/app/components/MedicineTable.tsx
+++ src/app/components/MedicineTable.tsx
@@ -1,88 +1,263 @@
 import * as React from "react";
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Close";
+import {
+  GridRowsProp,
+  GridRowModesModel,
+  GridRowModes,
+  DataGrid,
+  GridColDef,
+  GridToolbarContainer,
+  GridActionsCellItem,
+  GridEventListener,
+  GridRowId,
+  GridRowModel,
+  GridRowEditStopReasons,
+  GridValidRowModel,
+} from "@mui/x-data-grid";
+import { randomId } from "@mui/x-data-grid-generator";
 
 type MedicineTableProps = {
   query: string;
 };
 
 type MedicineType = {
+  id: string;
   ID: number;
   name: string;
   count: number;
-  date: Date;
+  joinDate: Date;
 };
-function createData({ ID, name, count, date }: MedicineType) {
-  return { ID, name, count, date };
-}
 
-const rows = [
-  createData({
+const initialRows: GridRowsProp = [
+  {
+    id: randomId(),
     ID: 1217,
     name: "Panadol",
-    count: 6,
-    date: new Date(),
-  }),
-  createData({
+    count: 25,
+    joinDate: new Date(),
+  },
+  {
+    id: randomId(),
     ID: 1218,
     name: "Tylenol",
-    count: 7,
-    date: new Date(),
-  }),
-  createData({
+    count: 4,
+    joinDate: new Date(),
+  },
+  {
+    id: randomId(),
     ID: 1219,
-    name: "Retinol",
-    count: 2,
-    date: new Date(),
-  }),
-  createData({
+    name: "Retinal",
+    count: 4,
+    joinDate: new Date(),
+  },
+  {
+    id: randomId(),
     ID: 1220,
-    name: "Strattera",
-    count: 2,
-    date: new Date(),
-  }),
-  createData({
-    ID: 1221,
-    name: "Adderall",
-    count: 4,
-    date: new Date(),
-  }),
-  createData({
-    ID: 1222,
-    name: "dextroamphetamine",
-    count: 4,
-    date: new Date(),
-  }),
+    name: "Strepsils",
+    count: 3,
+    joinDate: new Date(),
+  },
 ];
 
-const columns: GridColDef[] = [
-  { field: "ID", headerName: "ID", width: 100 },
-  { field: "name", headerName: "Name", width: 100 },
-  { field: "count", headerName: "Count", width: 100 },
-  { field: "date", headerName: "Date", minWidth: 300 },
-];
-
-function queryMatches({ query, ID, name }: MedicineType & MedicineTableProps) {
-  return name.includes(query) || String(ID).includes(query) || query === "";
+interface EditToolbarProps {
+  setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
+  setRowModesModel: (
+    newModel: (oldModel: GridRowModesModel) => GridRowModesModel
+  ) => void;
 }
 
-function filterRows(query: string) {
-  return rows.filter((row) => queryMatches({ query, ...row }));
+function EditToolbar(props: EditToolbarProps) {
+  const { setRows, setRowModesModel } = props;
+
+  const handleClick = () => {
+    const id = randomId();
+    setRows((oldRows) => [
+      ...oldRows,
+      { id, name: "", count: "", isNew: true },
+    ]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: { mode: GridRowModes.Edit, fieldToFocus: "name" },
+    }));
+  };
+
+  return (
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+        Add record
+      </Button>
+    </GridToolbarContainer>
+  );
 }
 
 export default function MedicineTable({ query }: MedicineTableProps) {
+  const [rows, setRows] = React.useState(initialRows);
+  const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>(
+    {}
+  );
+
+  function queryMatches({
+    query,
+    ID,
+    name,
+  }: Pick<MedicineType, "ID" | "name"> & MedicineTableProps) {
+    return name.includes(query) || String(ID).includes(query) || query === "";
+  }
+
+  function filterRows(query: string) {
+    return rows.filter((row) =>
+      queryMatches({ query, ID: row.ID, name: row.name })
+    );
+  }
+
+  const handleRowEditStop: GridEventListener<"rowEditStop"> = (
+    params,
+    event
+  ) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const handleEditClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id: GridRowId) => () => {
+    setRows(rows.filter((row) => row.id !== id));
+  };
+
+  const handleCancelClick = (id: GridRowId) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows.find((row) => row.id === id);
+    if (editedRow!.isNew) {
+      setRows(rows.filter((row) => row.id !== id));
+    }
+  };
+
+  const processRowUpdate = (newRow: GridRowModel) => {
+    const updatedRow = { ...newRow, isNew: false };
+    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    return updatedRow;
+  };
+
+  const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  const columns: GridColDef[] = [
+    { field: "ID", headerName: "ID", width: 80, editable: true },
+    { field: "name", headerName: "Name", width: 180, editable: true },
+    {
+      field: "count",
+      headerName: "Count",
+      type: "number",
+      width: 80,
+      align: "left",
+      headerAlign: "left",
+      editable: true,
+    },
+    {
+      field: "joinDate",
+      headerName: "Join date",
+      type: "date",
+      width: 180,
+      editable: true,
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      width: 100,
+      cellClassName: "actions",
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              key={`save-${id}`}
+              label="Save"
+              sx={{
+                color: "primary.main",
+              }}
+              onClick={handleSaveClick(id)}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              key={`cancel-${id}`}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            key={`edit-${id}`}
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            key={`delete-${id}`}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <Box
+      sx={{
+        height: 500,
+        width: "100%",
+        "& .actions": {
+          color: "text.secondary",
+        },
+        "& .textPrimary": {
+          color: "text.primary",
+        },
+      }}
+    >
       <DataGrid
         rows={filterRows(query)}
-        getRowId={(row) => row.ID}
         columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
+        editMode="row"
+        rowModesModel={rowModesModel}
+        onRowModesModelChange={handleRowModesModelChange}
+        onRowEditStop={handleRowEditStop}
+        processRowUpdate={processRowUpdate}
+        slots={{
+          toolbar: EditToolbar,
         }}
-        pageSizeOptions={[5, 10]}
+        slotProps={{
+          toolbar: { setRows, setRowModesModel },
+        }}
       />
-    </div>
+    </Box>
   );
 }
