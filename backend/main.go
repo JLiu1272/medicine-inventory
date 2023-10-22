02ba49c258b445c3920144b503b7fbcfb1b0728a
Jennifer Liu <jenniferliu1272@gmail.com>
2023-10-22 13:55:45 -0700

    Added endpoint /insertMedicine to Gin web server. Adds to DB

--- backend/main.go
+++ backend/main.go
@@ -1,43 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"api"
 
-	"db"
-
-	"go.mongodb.org/mongo-driver/bson"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	client, ctx, cancel, err := db.Connect("mongodb://localhost:27017")
-	if err != nil {
-		panic(err)
-	}
+	router := gin.Default()
+	router.GET("/insertMedicine", api.InsertToDB)
 
-	// Release resource when main function is returned.
-	defer db.Close(client, ctx, cancel)
-
-	// Create a object of type interface to store
-	// the bson values, that we are inserting into database.
-	document := bson.D{
-		{Key: "rollNo", Value: 175},
-	}
-
-	// insertOne accepts client , context, database
-	// name collection name and an interface that
-	// will be inserted into the collection.
-	// insertOne returns an error and a result of
-	// insert in a single document into the collection.
-	insertOneResult, err := db.InsertOne(client, ctx, "gfg",
-		"marks", document)
-
-	// handle the error
-	if err != nil {
-		panic(err)
-	}
-
-	// print the insertion id of the document,
-	// if it is inserted.
-	fmt.Println("Result of InsertOne")
-	fmt.Println(insertOneResult.InsertedID)
+	router.Run("localhost:8080")
 }
